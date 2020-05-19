0e49b37662a06057c003f840a1a035bfb6aa6724
martingratt <martin_gratt@hotmail.com>
2020-05-19 19:51:10 +0200

    Update Attack2_AccessibilityFeatures.ps1

--- Skripts/Attack2_AccessibilityFeatures.ps1
+++ Skripts/Attack2_AccessibilityFeatures.ps1
@@ -1,4 +1,13 @@
-﻿$path = "C:\prehendo\Logs\attack2.txt"
+<# 
+--------------------------------------------------------------------------------------------
+Die Verwendung der folgenden Skripte ist nur innerhalb der dafür vorgesehenen Testumgebung zu Lehr- oder Forschungszwecken erlaubt.
+Jeglicher Missbrauch zur Verursachung von Schaden oder missbräuchliche Verwendung mit bösartigen Absichten ist strengstens untersagt.
+Die Verantwortung liegt allein beim Anwender / der Anwenderin.
+Das Projektteam übernimmt keine Verantwortung für Schäden, die durch Dritte entstehen.
+--------------------------------------------------------------------------------------------
+#>
+
+$path = "C:\prehendo\Logs\attack2.txt"
 
 $ErrorActionPreference="SilentlyContinue"
 Stop-Transcript | out-null
